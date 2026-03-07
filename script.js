e1a59bdd7610ccb7560e94d11a49082835fc2b96
JohnsonDev14 <joaojbatatafrita@gmail.com>
2026-03-07 15:05:47 -0300

    site de media

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function mostraAlerta()
-{
-    alert("ACHOU QUE EU TAVA BRINCANDO");
+function media(){
+
+    var nota1 = parseFloat(document.getElementById("valor1").value)
+    var nota2 = parseFloat(document.getElementById("valor2").value)
+
+    var media = (nota1 + nota2) / 2
+
+    if(media >= 7){
+        alert(`Parabens ${media} é o suficiente pra passar`)
+    }
+    else{
+        alert(`vish ${media} não é o suficiente para passar`)
+    }
 }
 
-window.onload=function(){
-    let botaoAlerta = document.getElementById("ola");
-
-    botaoAlerta.onclick = function () {
-        mostraAlerta() ;
-    };
-}
